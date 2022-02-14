33a08008fd65031a0dc30eee267baa414379106b
chinsan <clim1994@gmail.com>
2022-02-14 09:57:03 -0800

    added onto drink cards and added a detail card
    co-authored by: joey joesphschang@gmail.com
    co-authored by: justin thejustinlombardi@gmail.com

--- src/app/menu/drink-card/drink-card.component.ts
+++ src/app/menu/drink-card/drink-card.component.ts
@@ -27,8 +27,8 @@ export class DrinkCardComponent implements OnInit {
     'orange peel, maraschino cherry',
     'none',
     'none',
-    'double shot',
-    'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/2/13/0/ED0309H_classic-manhattan-cocktail_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371614573383.jpeg')
+    'triple shot',
+    'https://www.liquor.com/thmb/lavmUGaMMriZPSxfHsPnyK15bSY=/720x720/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__liquor__2018__05__08113350__bourbon-old-fashioned-720x720-recipe-ade6f7780c304999be3577e565c9bcdd.jpg')
   ]
 
   constructor() { }
