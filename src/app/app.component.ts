f60ec0bb38676394ac0cf9afed2eda84e591a5dc
chinsan <clim1994@gmail.com>
2022-02-14 20:45:45 -0800

    fetch call works with heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'byob';
+  drinkList: any=[];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.fetchDrinks();
+  }
+
+  private fetchDrinks(){
+    this.http
+    .get('http://buildyobar.herokuapp.com/drinks/')
+    .subscribe(res => {
+      this.drinkList = res;
+      console.log(this.drinkList)
+    })
+  }
+
+
+
 }
--- src/app/profile/create-drink/create-drink.component.ts
+++ src/app/profile/create-drink/create-drink.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-drink',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateDrinkComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  OnCreateDrink(drinkData: {
+   name: string;
+   ice: string;
+   spirit: string;
+   liqueur: string;
+   juice: string;
+   garnish: string;
+   citrus: string;
+   soda: string;
+   special_request: string;
+   photo: string;
+  }){
+    this.http.post('http://buildyobar.herokuapp.com/drinks/', drinkData).subscribe(res => {
+      console.log(res)
+    })
   }
 
 }
