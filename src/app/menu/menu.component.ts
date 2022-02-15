91e7897ca38be8a92f5c557f69cdaef394d1690f
chinsan <clim1994@gmail.com>
2022-02-15 09:55:10 -0800

    implement params to almost display card details
    co-authored by: joey joesphschang@gmail.com
    co-authored by: justin thejustinlombardi@gmail.com

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
   constructor() { }
-   
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+
   }
 
 }
