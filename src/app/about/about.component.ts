b1da045a93213578e5e5753555bede60f997e63d
thejustinlombardi <thejustinlombardi@gmail.com>
2022-02-15 11:06:25 -0500

    Implement emailJs

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm('contact_service', 'template_rb90ig4', e.target as HTMLFormElement, 'user_PofRZkLPXasmqhS4pA0zj')
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
   constructor() { }
 
   ngOnInit(): void {
